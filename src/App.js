77288ac74c495fe8db87518c050d4ea452ebc1d7
TomBroager <U773553@rts-365.dk>
2022-02-01 13:50:07 +0100

    add component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Heading from "./Components/Heading";
+import Footer from "./Templates/Footer";
 
 function App() {
   return (
     <div className="App pt-9 px-6 h-screen bg-primary">
       <Heading />
+      <Footer />
     </div>
   );
 }
