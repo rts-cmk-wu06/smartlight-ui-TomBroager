706abe07839d93f7057d608c32fd81f307dea289
TomBroager <U773553@rts-365.dk>
2022-02-01 17:17:47 +0100

    initial commit

---
+++ src/Templates/MainContent.js
@@ -0,0 +1,9 @@
+const MainContent = () => {
+   return ( 
+      <main className="bg-themeColorTwo h-full rounded-t-3xl">
+         <h1 className="text-5xl">Main content</h1>
+      </main>
+    );
+}
+ 
+export default MainContent;
