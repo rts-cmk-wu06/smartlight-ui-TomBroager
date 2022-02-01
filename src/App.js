f2e8cdadd67e0373e54fa88686cdb1e97e1016ef
TomBroager <U773553@rts-365.dk>
2022-02-01 16:53:53 +0100

    add component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Heading from "./Components/Heading";
 import Footer from "./Templates/Footer";
+import Header from "./Templates/Header";
 
 function App() {
   return (
     <div className="App h-screen flex flex-col justify-between bg-themeColorOne">
       <div className="px-6 pt-8">
-       <Heading />
+       <Header />
       </div>
       <Footer />
     </div>
