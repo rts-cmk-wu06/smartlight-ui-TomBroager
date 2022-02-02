47e8f0867e3dd37360df7b1941e752d294383ab7
TomBroager <U773553@rts-365.dk>
2022-02-02 09:47:13 +0100

    add chnages

---
+++ src/Components/ProfileImage.js
@@ -0,0 +1,11 @@
+const ProfileImage = () => {
+   return ( 
+      <div className="h-14 w-14 border-2 border-solid border-themeColorWhite rounded-full overflow-hidden">
+         <img src="https://www.seekpng.com/png/detail/122-1228340_cute-shark-png-shark-drawing-cartoon.png"
+         alt="" className="object-cover"/>
+         {/* <div className="w-full h-full bg-black"></div> */}
+      </div>
+    );
+}
+ 
+export default ProfileImage;
--- src/Templates/Header.js
+++ src/Templates/Header.js
@@ -1,8 +1,12 @@
 import Heading from "../Components/Heading";
+import ProfileImage from "../Components/ProfileImage";
 
 const Header = () => {
    return ( 
+      <header className="flex justify-between items-center">
       <Heading />
+      <ProfileImage />
+      </header>
     );
 }
  
