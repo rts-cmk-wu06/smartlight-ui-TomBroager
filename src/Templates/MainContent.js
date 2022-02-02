757ff2f3cb6626030efb3947009526dbad6a7ec1
TomBroager <U773553@rts-365.dk>
2022-02-02 10:10:03 +0100

    add heading and grid

--- src/Templates/MainContent.js
+++ src/Templates/MainContent.js
@@ -1,7 +1,10 @@
 const MainContent = () => {
    return ( 
-      <main className="bg-themeColorTwo rounded-t-3xl">
-         <h1 className="text-5xl">Main content</h1>
+      <main className="bg-themeColorTwo rounded-t-3xl px-6 py-6">
+         <h2 className="text-lg font-bold">All Rooms</h2>
+         <div className="grid grid-cols-2 gap-6">
+
+         </div>
       </main>
     );
 }
